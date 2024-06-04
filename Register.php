383a424fb01fd63c5a9522d54c5088a4f657db62
DimasPutra28 <dimas.meidiansyah.putra-2021@vokasi.unair.ac.id>
2024-06-05 02:25:29 +0700

    First

---
+++ Register.php
@@ -0,0 +1,165 @@
+<?php
+
+require_once("local.php");
+
+if(isset($_POST['register'])){
+
+    $nama = filter_input(INPUT_POST, 'nama');
+    $tgldaftar = filter_input(INPUT_POST, 'tgldaftar');
+    $user = filter_input(INPUT_POST, 'user');
+    $pass = password_hash($_POST["pass"], PASSWORD_DEFAULT);
+
+
+    $sql = "INSERT INTO user (nama, tgldaftar, user, pass) 
+            VALUES (:nama, :tgldaftar, :user, :pass)";
+    $stmt = $db->prepare($sql);
+
+    $params = array(
+        ":nama" => $nama,
+        ":tgldaftar" => $tgldaftar,
+        ":user" => $user,
+        ":pass" => $pass
+    );
+
+    $saved = $stmt->execute($params);
+
+
+    if($saved) header("Location: login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Drive Thru Perpustakaan Arcadia</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Spinner Start -->
+    <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+        <div class="spinner-grow text-primary" style="width: 3rem; height: 3rem;" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+    </div>
+    <!-- Spinner End -->
+
+
+    <!-- Topbar Start -->
+    
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0 wow fadeIn" data-wow-delay="0.1s">
+        <a href="index.html" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
+            <h3 class="m-0 ">Drive Thru Perpustakaan Arcadia</h3>
+        </a>
+        <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+       
+    </nav>
+    <!-- Navbar End -->
+
+
+    <!-- Header Start -->
+    <div class="container-fluid header bg-primary p-0 mb-5"  style="background-color: white;">
+        <div class="row "  style="background-color: white;">
+            <h4 class="" style="display:  flex;justify-content: center;margin-top: 150px;text-align: center;padding: 10px;" >Pendaftaran Akun</h4>
+            <p class="" style="display:  flex;justify-content: center;" >Silahkan Daftar </p>
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4">
+                <form action="" method="POST">
+                    
+                    <div class="form-group">
+                        <label for="name">Nama Lengkap</label>
+                        <input class="form-control" type="text" name="nama" placeholder="Nama kamu" />
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="username">Tanggal Daftar</label>
+                        <input class="form-control" type="date" name="tgldaftar" placeholder="" />
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="email">Username</label>
+                        <input class="form-control" type="text" name="user" placeholder="" />
+                    </div>
+                    
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input class="form-control" type="password" name="pass" placeholder="Password" />
+                    </div>
+                    
+                    <a href="login.php"><input type="submit" class="btn btn-success btn-block" name="register" value="Daftar"  style="background-color: black;color: white;margin-top: 20px;display: flex;justify-content: center;text-align:center;width: 640px;"/></a>
+                </div>
+                <div class="col-lg-4"></div>
+        </div>
+    </div>
+    <!-- Header End -->
+
+
+    <!-- About Start -->
+    <div class="container-xxl py-2">
+        <div class="container">
+            
+        </div>
+    </div>
+    <!-- About End -->
+
+
+
+  
+
+
+
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square rounded-circle back-to-top"><i class="bi bi-arrow-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/tempusdominus/js/moment.min.js"></script>
+    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
